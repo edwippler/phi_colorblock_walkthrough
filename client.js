abfc8d55d81efe9b686f8240597358ffdf89ba5e
Edmund Wippler <edmund@Edmunds-MacBook-Pro.local>
2017-02-15 11:11:28 -0600

    base mode completed

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
+var colorArray = ['AliceBlue', 'AntiqueWhite', 'CadetBlue', 'Aqua', 'Chartreuse'];//array of colors
+var randomColor = '';
+
 $(document).ready(function(){
   // console.log('jquery is ready to go!');//log to say we're ready
-  var colorArray = ['AliceBlue', 'AntiqueWhite', 'CadetBlue', 'Aqua', 'Chartreuse'];//array of colors
 
   // loops through all the colors
   for (var i = 0; i < colorArray.length; i++) {
@@ -16,19 +18,26 @@ $(document).ready(function(){
     console.log($(this).data().colorOfBlock);
     var colorOfBlockSelected = $(this).data().colorOfBlock;
     if (randomColor == colorOfBlockSelected){
+      //the user got it right
       $("#responseSection").text('You got it!');
+      chooseNewRandomColor(); 
     }else {
       $('#responseSection').text('Oh no...that is not right')
     }
-  })
-  // select a random color and add it to the DOM
-  var randomNumberSelected = randomNumber(0, colorArray.length - 1); //selects a random number
-  var randomColor = colorArray[randomNumberSelected]; //uses the random number to select a number form the array
-  $('#userColorPrompt').text(randomColor);
+  });
 
+  chooseNewRandomColor();
 
 });
 
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function chooseNewRandomColor() {
+  // select a random color and add it to the DOM
+  var randomNumberSelected = randomNumber(0, colorArray.length - 1); //selects a random number
+  randomColor = colorArray[randomNumberSelected]; //uses the random number to select a number form the array
+  $('#userColorPrompt').text(randomColor);
+
 }
