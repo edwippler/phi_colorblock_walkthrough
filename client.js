b50558918568b8ec43a1a626045fe4a749ca46c9
Edmund Wippler <edmund@Edmunds-MacBook-Pro.local>
2017-02-15 10:28:17 -0600

    added random color to page

--- client.js
+++ client.js
@@ -10,5 +10,17 @@ $(document).ready(function(){
     newColorBlock.addClass('colorBlock'); //adds a class to the empty div - gives height & width
     $('#colorBlockContainer').append(newColorBlock); //add the div to the DOM.
   }
+
+
+  // select a random color and add it to the DOM
+  var randomNumberSelected = randomNumber(0, colorArray.length - 1); //selects a random number
+  var randomColor = colorArray[randomNumberSelected]; //uses the random number to select a number form the array
+  $('#userColorPrompt').text(randomColor);
+
   
+
 });
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
